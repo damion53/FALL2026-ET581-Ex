eb0259453047034be28a22eecfde092b050f64ae
damion53 <damion.south53@login.cuny.edu>
2026-09-08 23:19:16 +0000

    print format test commit

---
+++ Lab1/InputTest.java
@@ -0,0 +1,7 @@
+public class InputTest {
+    public static void main =(String[] args) {
+        Scanner input = new Scanner(System.in); // creating a scanner object called input
+        int x ; // declare a car storing user input
+        x = input.next
+    }
+}
---
+++ Lab1/PrintFormatTest.java
@@ -0,0 +1,8 @@
+    public class PrintFormatTest {
+        public static void main(String[] args) {
+
+
+            System.out.printf(" Hello \t: %\n", "Li"); // String value
+            // \t tab char \n new line char
+        }
+    }
